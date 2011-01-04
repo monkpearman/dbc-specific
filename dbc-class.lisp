b3c76e76cdc6cacb777971e3b69af3a97835033c
mon_key <monkey@sandpframing.com>
2011-01-04 15:29:43 -0500

    adding new directory to hold versioned note related files

--- dbc-class.lisp
+++ dbc-class.lisp
@@ -1,7 +1,45 @@
+;;-*- Mode: LISP; Syntax: COMMON-LISP; Encoding: utf-8; Base: 10 -*-
 ;;; :FILE-CREATED <Timestamp: #{2011-01-01T14:10:05-05:00Z}#{11526} - by MON>
 ;;; :FILE /home/sp/HG-Repos/CL-repo-HG/CL-MON-CODE/dbc-specific/dbc-class.lisp
 ;;; ==============================
 
+
+(in-package #:dbc)
 
+
+;;; ==============================
+;; :ARTIST-INFOS
+;; <table_structure name="artist_infos">
+;; <field Field="bio" Type="int(10) unsigned" Null="NO" Key="" Default="0" Extra="" />
+;; <field Field="id" Type="int(4) unsigned" Null="NO" Key="PRI" Extra="auto_increment" />
+;; <field Field="display" Type="varchar(255)" Null="NO" Key="" Default="" Extra="" />
+;; <field Field="nationality" Type="varchar(255)" Null="NO" Key="" Default="" Extra="" />
+;; <field Field="role" Type="varchar(255)" Null="NO" Key="" Default="" Extra="" />
+;; <field Field="lifespan" Type="varchar(100)" Null="NO" Key="" Default="" Extra="" />
+;; <field Field="date_born" Type="varchar(255)" Null="NO" Key="" Default="" Extra="" />
+;; <field Field="date_died" Type="varchar(255)" Null="NO" Key="" Default="" Extra="" />
+;; <field Field="birth_location" Type="varchar(255)" Null="NO" Key="" Default="" Extra="" />
+;; <field Field="death_location" Type="varchar(255)" Null="NO" Key="" Default="" Extra="" />
+;; <field Field="LOC_control" Type="varchar(15)" Null="YES" Key="" Default="0" Extra="" />
+;; <field Field="used_for" Type="text" Null="NO" Key="" Extra="" />
+;; <field Field="found_in" Type="text" Null="NO" Key="" Extra="" />
+;; <field Field="appeared_in" Type="text" Null="NO" Key="MUL" Extra="" />
+;; <field Field="ads_for" Type="text" Null="NO" Key="" Extra="" />
+;; <field Field="auction_records" Type="text" Null="NO" Key="" Extra="" />
+;; <field Field="cancel_num" Type="int(10) unsigned" Null="NO" Key="" Default="0" Extra="" />
+;; <field Field="special_note" Type="text" Null="NO" Key="" Extra="" />
+;; <field Field="gender" Type="varchar(255)" Null="NO" Key="" Default="" Extra="" />
+;; <field Field="date_edit" Type="varchar(255)" Null="NO" Key="" Default="" Extra="" />
+;; <field Field="ULAN_control" Type="varchar(15)" Null="YES" Key="" Extra="" />
+;; <field Field="default_pic" Type="varchar(10)" Null="NO" Key="" Default="" Extra="" />
+;; <field Field="print_default_pic" Type="varchar(10)" Null="NO" Key="" Default="" Extra="" />
+;; <field Field="also_author" Type="tinyint(3) unsigned" Null="NO" Key="" Default="0" Extra="" />
+;; <field Field="also_people" Type="tinyint(3) unsigned" Null="NO" Key="" Default="0" Extra="" />
+;; <field Field="user_name" Type="varchar(32)" Null="YES" Key="" Extra="" />
+;; <field Field="naf_creator" Type="varchar(32)" Null="YES" Key="" Extra="" />
+;; <field Field="online" Type="tinyint(3) unsigned" Null="NO" Key="" Default="0" Extra="" />
+;; <field Field="date_edt" Type="timestamp" Null="NO" Key="" Default="CURRENT_TIMESTAMP" Extra="on update CURRENT_TIMESTAMP" />
+
+
 ;;; ==============================
 ;;; EOF
