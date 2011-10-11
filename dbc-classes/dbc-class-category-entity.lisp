8d4159c61898d711a6732191b49c67961383cd40
MON KEY <monkey@sandpframing.com>
2011-10-11 15:47:38 -0400

    New files dbc-classes/dbc-class-category-entity.lisp, dbc-classes/dbc-class-naf-entity.lisp

---
+++ dbc-classes/dbc-class-category-entity.lisp
@@ -0,0 +1,18 @@
+;;; :FILE-CREATED <Timestamp: #{2011-10-11T15:20:06-04:00Z}#{11412} - by MON>
+;;; :FILE dbc-specific/dbc-classes/dbc-class-category-entity.lisp
+;;; ==============================
+
+;; *control-id-display-category-namespace*
+;; --- control-id-category-entity-type           (control-id-entity-type) 
+;; ---- control-id-category-entity               (control-id-category-entity-type)
+;; ----- control-id-category-entity-display-name (control-id-category-entity control-id-display-name-for-entity-type)
+;; ------ control-id-display-category            (control-id-category-entity-display-name)
+
+(in-package #:dbc)
+
+(defclass category-entity (base-category-entity)
+  ()
+  )
+
+;;; ==============================
+;;; EOF
---
+++ dbc-classes/dbc-class-naf-entity.lisp
@@ -0,0 +1,39 @@
+;;; :FILE-CREATED <Timestamp: #{2011-10-11T14:49:49-04:00Z}#{11412} - by MON>
+;;; :FILE dbc-specific/dbc-classes/dbc-class-naf-entity.lisp
+;;; ==============================
+
+(in-package #:dbc)
+
+(defclass naf-entity-artist (base-naf-entity)
+  ;; control-id-display-artist
+  ;; *control-id-display-artist-namespace*
+  ()
+  )
+
+(defclass naf-entity-author (base-naf-entity)
+  ;; control-id-display-author
+  ;; *control-id-display-author-namespace*
+  ()
+  )
+
+(defclass naf-entity-person (base-naf-entity)
+  ;; control-id-display-person
+  ;; *control-id-display-person-namespace*
+  ()
+  )
+
+(defclass naf-entity-brand (base-naf-entity)
+  ;; control-id-display-brand
+  ;; *control-id-display-brand-namespace*
+  ()
+  )
+
+(defclass naf-entity-publication (base-naf-entity)
+  ;; control-id-display-publication
+  ;; *control-id-display-publication-namespace*
+  ()
+  )
+
+
+;;; ==============================
+;;; EOF
