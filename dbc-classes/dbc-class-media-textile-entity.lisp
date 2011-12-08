bca77d81822346da0bdaab3b8e9f0142cb810cd7
MON KEY <monkey@sandpframing.com>
2011-12-07 21:24:40 -0500

    New File: dbc-classes/dbc-class-media-textile-entity.lisp

---
+++ dbc-classes/dbc-class-media-textile-entity.lisp
@@ -0,0 +1,209 @@
+;;; :FILE-CREATED <Timestamp: #{2011-10-04T14:37:50-04:00Z}#{11402} - by MON>
+;;; :FILE dbc-specific/dbc-classes/dbc-class-media-textile-entity.lisp
+;;; ==============================
+
+(in-package #:dbc)
+
+#|
+
+
+(defclass mitchell-and-church 
+    ((display-name
+      )
+     (printed-annotation
+      )
+     (width
+      )
+     (id-number
+      )
+     (price-per-yard-original
+      )
+     (price-per-yard-annotation
+      )
+     (marked-sold-out
+      )
+     (marked-other-annotation
+     ))
+  )
+
+
+textile -- weave | print
+           synthetic | natural
+
+ - WOVEN, PRINT, SYNTHETIC, NATUURAL are _aspects_ of a textile 
+   (not necessarily mutually exclusive)
+
+;; ==============================
+
+:TODO further classify these
+TEXTILE-TERMS 
+
+;; :FIBER-TYPE
+alpaca
+cotton
+linen
+rayon
+silk
+
+
+batiste
+broadcloth
+brocade
+chambray
+challie ;; also appears as "challis"
+check
+chiffon
+cord ;; "carolan cord" -- maybe for cordray?
+crash ;; "crash toweling" "linen crash" "linen weft crash"
+crepe
+damask
+dimity
+fainne ;; "embroidered fainne"
+flannel
+foulard
+gabardine
+georgette
+gingham
+homespun
+jacquard
+organdie
+pongee
+poplin
+satin
+scrim
+shantung
+taffeta
+tussah
+tweed
+twill
+velvet
+velveteen
+voile
+worsted
+
+;; :NOTE are these a M&C "brand"
+charmeuse 
+cordray
+marquisette
+ormonde
+percale ;; english | wales
+soiesette 
+lindy  ;; loox | havana
+
+;; :TREATMENT
+mercererized
+
+;; :USAGE
+
+crash
+drapery
+lingerie
+suiting
+toweling ;; also as "glass toweling" and "crash toweling"
+shirting
+pajama ;; "pajama check"
+
+;; :SUPPLIERS/LINE
+amelia ;; "amelia silk"
+arnaud ;; "arnaud rayon fabric" -- occurs at least three times
+ascot ;; "ascot broadcloth" -- could be literally "broadcloth for an ascot"
+amundsen ;; flannel
+bretonne ;; chambray
+brookfield ;; "brookfield charmeuse"
+byrde ;; "byrde linene"
+canton ;; "canton brocade" -- "canton crepe" 2x
+carolan ;; "carolan cord"
+celanese ;; checks
+chester ;; suiting | novelty
+clarendon ;; challie
+claremont ;; "claremont poplin"
+clifton
+crestwood ;; fabric
+dartmouth ;; "darmouth print" "dartmouth flannel"
+dover ;; "dover flannel"
+delia ;; "delia prints"
+essex ;; "essex twill"
+foxcroft ;; flannel
+granite ;; "granite cloth"
+grenada ;; "grenada gingham"
+hampshire ;; "hampshire gingham"
+havana ;; "havana lindy"
+hawthorne ;; as "hawthorne"
+kenwood
+kingston
+loox ;; "loox lindy"
+lorraine ;; gingham
+lubec ;; "lubec chambray" 
+lustra ;; "lustra check" - maybe a rayon/nylon substitute for alpaca?
+nantucket ;; "nantucket fabric"
+norfolk ;; "norfolk broadcloth"
+madelaine ;; "madelaine voile" - also as "crepe madelaine" 2x
+melrose  ;; "melrose marquisette" "melrose flannel"
+montrose ;; "montrose scrim"
+middlesex ;; gingham
+nepnoset ;; gingham
+palmetto ;; "palmetto prints"
+peerless ;; "peerless venetian" -- maybe for the motor car?
+pembroke ;; gingham
+pershing ;; "pershing suiting"
+picardy ;; "picardy suiting"
+pinecrest ;; "pinecrest novelty"
+priscilla ;; "priscilla rayon"
+rakdek ;; "raydek dimity"
+raycot ;; "raycot print" - maybe printed rayon? example was _not_ cotton.
+raydex ;; "raydex tweed"
+riviera ;; flannel
+rodier ;; "rodier voile"
+rosilyn ;; "rosilyn shantung"
+saracene ;; flannel
+scarsdale ;; gingham
+somerset ;; gingham
+southdown ;; challie
+stratford ;; suiting
+suntweed ;; suiting
+vernon  ;; "vernon voile"
+victoria ;; "victoria challis" "victoria crepe"
+wiscasset ;; "wiscasset rayon"
+sylvasilk ;; "sylvasilk sateen" -- 4x
+
+;-- 
+argent ;; "argent crepe rayon" -- 2x
+belmont ;; "belmont crepe" -- 2x
+canton ;; "canton crepe" -- 2x
+chalons ;; "chalons crepe"
+deauville ;; "deauville crepe"
+echo ;; "echo crepe"
+elsie ;; "elsie crepe"
+elwood ;; "elwood crepe"
+fan tan ;; "fan tan crepe" -- 2x
+georgeen ;; "georgeen crepe"
+glenadore ;; "glenadore crepe"
+ko ko ; "ko ko crepe"
+laurel ;; "laurel silk crepe"
+madelaine ;; "crepe madelaine" -- 2x
+magnolia ;; "magnolia crepe" -- 2x
+mallinson ;; "mallinson crepe"
+myrtle ;; "myrtle crepe" -- 2x
+newport ;; "newport crepe" -- 2x
+renault ;; "renault crepe" -- 2x
+seltex ;; "seltex crepe"
+thebes ;; "thebes crepe"
+vassar ;; "vassar crepe" -- 9x
+victoria ;; "victoria crepe"
+willamette ;; "willamette canton crepe" -- 7x
+
+;; :NOTE Do annotations at top right corner indicate a minimum order?
+
+|#
+
+;;; ==============================
+
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; show-trailing-whitespace: t
+;; mode: lisp-interaction
+;; End:
+
+;;; ==============================
+;;; EOF
