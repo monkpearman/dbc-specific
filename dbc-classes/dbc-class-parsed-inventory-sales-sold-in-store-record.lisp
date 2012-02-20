dde9a37df3353c6daf53b68fca1f15c4039ac3d7
MON KEY <monkey@sandpframing.com>
2012-02-20 18:01:03 -0500

    Added new class definition for class `parsed-inventory-sales-sold-in-store-record' added forms evaluating to the functions: `set-parsed-inventory-sales-sold-in-store-record-slot-value', `parsed-inventory-sales-sold-in-store-record-xml-dump-file-and-hash'.

--- dbc-classes/dbc-class-parsed-inventory-sales-sold-in-store-record.lisp
+++ dbc-classes/dbc-class-parsed-inventory-sales-sold-in-store-record.lisp
@@ -25,18 +25,60 @@ These contents of these XML files correspond to the following derbycityprints SQ
 ;;     :accessor <SLOT-ACCESSOR>
 ;;     :documentation ":ORIGINAL-FIELD \"<ORIGINAL-STRING>\"")
 ;; 
-;; (defclass parsed-inventory-sales-sold-in-store-record (parsed-class)
-;;  (()))
+(defclass parsed-inventory-sales-sold-in-store-record (parsed-class)
+  ((ignorable-sold-in-store-id-number
+    :initarg :ignorable-sold-in-store-id-number
+    :accessor ignorable-sold-in-store-id-number
+    :documentation ":ORIGINAL-FIELD \"id\"")
+   (price-sold-in-store
+    :initarg :price-sold-in-store
+    :accessor price-sold-in-store
+    :documentation ":ORIGINAL-FIELD \"price\"")
+   ( ;; control-id-indexed-inventory-record
+    inventory-number
+    :initarg :inventory-number
+    :accessor inventory-number
+    :documentation ":ORIGINAL-FIELD \"ref\"")
+   (description-inventory-sold-in-store-comment
+    :initarg :description-inventory-sold-in-store-comment
+    :accessor description-inventory-sold-in-store-comment
+    :documentation ":ORIGINAL-FIELD \"comment\"")
+   (sold-in-store-date
+    :initarg :sold-in-store-date
+    :accessor sold-in-store-date
+    :documentation ":ORIGINAL-FIELD \"date\""))
+  (:documentation 
+   #.(format nil
+             "Class for parsed dbc xml `sold_in_store` table.~%~@
+:EXAMPLE ~%
+ \(mon:class-slot-list  'parsed-inventory-sales-order-record\)~%~@
+:NOTE the accessor `inventory-number' should be used as value for the
+KEY-ACCESSOR keyword of `load-sax-parsed-xml-file-to-parsed-class-hash'.~%
+:SEE-ALSO `set-parsed-inventory-sales-sold-in-store-record-slot-value', ~
+`parsed-inventory-sales-sold-in-store-record-xml-dump-file-and-hash'.")))
 
-;; (make-parsed-class-field-slot-accessor-mapping 
-;;  'parsed-inventory-sales-sold-in-store-record
-;;  '((<MATCH-STRING> . <TRANSFORM>)
-;;    (<MATCH-STRING> . <TRANSFORM>)
-;;    )))
+
+(make-parsed-class-field-slot-accessor-mapping
+ 'parsed-inventory-sales-sold-in-store-record
+ '(("id"       . ignorable-sold-in-store-id-number)
+   ("price"    . price-sold-in-store)
+   ("ref"      . inventory-number)
+   ("comment"  . description-inventory-sold-in-store-comment)
+   ("date"     . sold-in-store-date)))
+
+;; `parsed-inventory-sales-sold-in-store-record-xml-dump-file-and-hash'
+;; (parsed-inventory-sales-sold-in-store-record-xml-dump-file-and-hash)
+(def-parsed-class-record-xml-dump-file-and-hash
+    :parsed-class parsed-inventory-sales-sold-in-store-record
+  :default-key-accessor inventory-number
+  :default-input-pathname-name "sold-in-store-xml"
+  :default-output-pathname-sub-directory (list "parsed-xml-inventory-sales-sold-in-store-record")
+  :default-output-pathname-base-directory (sub-path *xml-output-dir*)
+  :default-output-pathname-name "sold-in-store-records")
 
 
 ;;; ==============================
-;; :FIELD "id" :TRANSFORM
+;; :FIELD "id" :TRANSFORM ignorable-sold-in-store-id-number
 ;;
 ;;         :TYPE "mediumint(8) unsigned"
 ;;         :NULL-P "NO"
@@ -45,12 +87,10 @@ These contents of these XML files correspond to the following derbycityprints SQ
 ;;
 ;; :EXAMPLE-VALUES 
 ;;
-;;
-;; -
-;;
+;; - originally the primary key, but we've verified that each inventory-number is unique.
 
 ;;; ==============================
-;; :FIELD "price" :TRANSFORM
+;; :FIELD "price" :TRANSFORM price-sold-in-store
 ;;
 ;;         :TYPE "mediumint(9)"
 ;;         :NULL-P "NO"
@@ -59,13 +99,14 @@ These contents of these XML files correspond to the following derbycityprints SQ
 ;;         :EXTRA ""
 ;;
 ;; :EXAMPLE-VALUES 
+;;  0 | positive-integer
 ;;
-;;
-;; -
+;; - never contains floating points. 
+;; price-ask, price-sold-ebay, 
 ;;
 
 ;;; ==============================
-;; :FIELD "ref" :TRANSFORM
+;; :FIELD "ref" :TRANSFORM inventory-number
 ;;
 ;;         :TYPE "mediumint(8) unsigned"
 ;;         :NULL-P "NO"
@@ -76,11 +117,12 @@ These contents of these XML files correspond to the following derbycityprints SQ
 ;; :EXAMPLE-VALUES 
 ;;
 ;;
-;; -
-;;
+;; - control-id-indexed-inventory-record
+;; - Effectively a primary key, all field values are unique and we commented out
+;;   the sole record with a non-sensical ref number (it was "0")
 
 ;;; ==============================
-;; :FIELD "comment" :TRANSFORM
+;; :FIELD "comment" :TRANSFORM description-inventory-sold-in-store-comment
 ;;
 ;;         :TYPE "text"
 ;;         :NULL-P "NO"
@@ -88,13 +130,15 @@ These contents of these XML files correspond to the following derbycityprints SQ
 ;;         :EXTRA ""
 ;;
 ;; :EXAMPLE-VALUES 
+;;  sold with 8723
+;;  sold with 298
+;;  Bought with China map and Misc 067
 ;;
-;;
-;; -
-;;
+;; - empty or a comment regarding the sale.
+;; - description-inventory-condition
 
 ;;; ==============================
-;; :FIELD "date" :TRANSFORM
+;; :FIELD "date" :TRANSFORM sold-in-store-date
 ;;
 ;;         :TYPE "varchar(255)"
 ;;         :NULL-P "NO"
@@ -103,10 +147,14 @@ These contents of these XML files correspond to the following derbycityprints SQ
 ;;         :EXTRA ""
 ;;
 ;; :EXAMPLE-VALUES 
+;;  "--"         ; replaced by "0000-00-00 00:00:00"
+;;  "01-27-2005" ; replaced by "2005-01-27 00:00:00"
+;;  
 ;;
-;;
-;; -
-;;
+;; - Note the date format MM-DD-YYYY was not congruent with our preferred form
+;;   and the "--" was not commentable XML b/c the token "--" can't occur inside a comment.
+;; payment-authorization-manual-verification-date 
+
 ;;; ==============================
 ;; `sold_in_store`
 ;;
