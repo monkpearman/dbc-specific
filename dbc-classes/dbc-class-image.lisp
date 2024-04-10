edf74cfe9532e66142a2c4405296d81042c2c7f2
S Pearman <monkpearman@icloud.com>
2024-04-10 15:13:13 -0400

    frob documentation formattting

--- dbc-classes/dbc-class-image.lisp
+++ dbc-classes/dbc-class-image.lisp
@@ -3,17 +3,17 @@
 ;;; ==============================
 
 ;;; ==============================
-;; :SOURCE #lisp slyrux regarding opticl :DATE 2011-06-22T21:00:20-04:00Z 
-;; (defun make-thumbnail-image (file image &key (y 128) (x 128) (pad t)) 
+;; :SOURCE #lisp slyrux regarding opticl :DATE 2011-06-22T21:00:20-04:00Z
+;; (defun make-thumbnail-image (file image &key (y 128) (x 128) (pad t))
 ;;   (write-image-file file (fit-image-into image y x :pad pad)))
 ;; :EXAMPLE
-;; (make-thumbnail-image 
+;; (make-thumbnail-image
 ;;  "/tmp/seahorse-thumb.png"
-;;  (read-jpeg-stream 
-;;   (drakma:http-request "http://photos.cyrusharmon.org/img/s8/v12/p497325516-6.jpg" 
-;;                        :want-stream t 
+;;  (read-jpeg-stream
+;;   (drakma:http-request "http://photos.cyrusharmon.org/img/s8/v12/p497325516-6.jpg"
+;;                        :want-stream t
 ;;                        :force-binary t)))
-;;; ==============================                                         
+;;; ==============================
 
 
 ;;; ==============================
@@ -22,7 +22,7 @@
 ;;; Every image directory in LV-NEF-DRIVE-B should have a dedicated namespace.
 ;;; Every image in each of these directories should get an embedded
 ;;; exif/xmp/IPTC v5-UUID with the directory as its source namespace.
-;;; :SEE `*control-id-image-namespace*' in 
+;;; :SEE `*control-id-image-namespace*' in
 ;;; :FILE dbc-specific/uuids-loadtime-bind.lisp
 ;;; ==============================
 
@@ -48,7 +48,7 @@ be considered w/r/t dbc images and their initial characteristics:
      --- dbc images getting incorporated into the system
      --- images related to an object in the dbc system but not in the system
      ---- images residion on a path in the local file system, e.g. naf images
-     ---- images online, e.g. an ebay item 
+     ---- images online, e.g. an ebay item
 
  - Those that don't exist yet but which soon will and have known properties.
     This will most often happen whenever a new item or entity is created.
@@ -62,23 +62,23 @@ be considered w/r/t dbc images and their initial characteristics:
 dbc-image (base-image)
 
 :DBC-IMAGE-SLOTS
-image-path-for-instance-type    
-  i.e. a relative pathname for an images type: 
+image-path-for-instance-type
+  i.e. a relative pathname for an images type:
    A naf-entity might go in /some/path/naf/<some-naf-instance>/some.jpg
    A category enity might go in /some/path/categs/categ-N/<some-categ-instance>/some-categ.gif
 
-image-path-for-type 
-  i.e. <IMAGE-TYPE-PATH>/<SOME-INSTANCE>/ 
+image-path-for-type
+  i.e. <IMAGE-TYPE-PATH>/<SOME-INSTANCE>/
   It isn't clear how/whether image-type-path and image-dir relate.
 
-image-type {jpg | png | gif | tiff | bmp | nef } 
+image-type {jpg | png | gif | tiff | bmp | nef }
   An image file extension, i.e. a CL:PATHNAME-TYPE.
-  What about svg jpeg. ppm  -- pnm pbm 
-  What about pdf? 
+  What about svg jpeg. ppm  -- pnm pbm
+  What about pdf?
   Note make sure that case-sensitivity never becomes and and all extensions are
   verified against a canonical lookup.
   :SEE http://www.martinreddy.net/gfx/2d/BMP.txt
-  Note see dublin core format 
+  Note see dublin core format
 
 image-size          {:type integer :display [:MB|:KB]}
 
@@ -106,13 +106,13 @@ image-orientation-whole
 ;; orientation of the surrounding context.
 
 image-orientation-depicted
-                     ;; { BOOLEAN [ :horizontal | :vertical ] } 
+                     ;; { BOOLEAN [ :horizontal | :vertical ] }
 
 image-dimension-x   {:type postive-integer}
-                   ;; The images x axis 
+                   ;; The images x axis
 
 image-dimension-y   {:type postive-integer}
-                   ;; The images y axis 
+                   ;; The images y axis
 
 
 :NOTE Following orientation related methods specialized on persistent-class bknr.images:store-image
@@ -145,8 +145,8 @@ Maybe consider adding slots/methods with names:
      image-frame  (dbc-image)
      image-gif    (dbc-image)
      image-ico    (dbc-image)
-     image-exif   (dbc-image) 
-        :NOTE Can inherit or used this to deterime slot values above. 
+     image-exif   (dbc-image)
+        :NOTE Can inherit or used this to deterime slot values above.
         :NOTE Also, that exif date/time stamps are mutable whereas a image-timestamp isn't
      image-xmp   (dbc-image)
        Like image-exif -- Adobe's "Extensible Metadata Platform"
@@ -156,7 +156,7 @@ image-xref (dbc-image)
  image-xrefs      ;; Table of instances/uuids which image might xref to.  :NOTE the semantics of xrefing are still not clear
                   ;; :SEE The  "print_default_pic" :TRANSFORM "image-default-xref" in dbc-class-artist-convert.lisp
  image-xrefed-by  ;; Table of instances/uuids which xref image.
- 
+
 
 image-site (dbc-image)
  gif-site   (image-gif)
@@ -200,9 +200,9 @@ image-theme-entity
 ;; :IMAGES
 
 ;; path related stuff will likely pivot on both the value of image-type image-type-path
-;; image-path 
+;; image-path
 ;; (setf image-path)
-;; image-verify-path 
+;; image-verify-path
 
 ;; image-resize {image-thumb image-big image-header image-flash image-zoom image-frame}
 ;;  `-> image-grow
@@ -212,7 +212,7 @@ image-theme-entity
 ;; image-verify-rotation
 
 ;; image-watermark
-;; 
+;;
 
 ;; image-metadata ;; exif metadata getter
 ;; (setf image-metadata) ;; exif metadata setter
@@ -239,14 +239,14 @@ image-theme-entity
 ;;  :NOTE using metafs with inotify could trigger updating the medatada should it ever change on disk.
 ;;
 ;;; ==============================
-;; 
+;;
 ;; :NOTE CL-FUSE and QueryFS might present a good bridge for implementing a filesystem indexer for sidecar metadata like .xmp and .mie
 ;; :SEE (URL `http://mtn-host.prjek.net/projects/cl-fuse/files/query-fs.pdf')
 ;; :SEE (URL `http://sourceforge.net/apps/mediawiki/fuse/index.php?title=FileSystems') 
 ;;
 ;; :NAME CL-FUSE
 ;; :AUTHOR Michael Raskin
-;; :HOMEPAGE http://mtn-host.prjek.net/projects/cl-fuse/ 
+;; :HOMEPAGE http://mtn-host.prjek.net/projects/cl-fuse/
 ;; :DESCRIPTION
 ;;
 ;; ,----
@@ -274,14 +274,14 @@ image-theme-entity
 ;; :SEE (URL `http://www.iptc.org/std/Iptc4xmpCore/1.0/specification/Iptc4xmpCore_1.0-spec-XMPSchema_8.pdf')
 ;; :SEE (URL `http://www.iptc.org/std/photometadata/2008/specification/IPTC-PhotoMetadata-2008_2.pdf')
 ;; :SEE (URL `http://www.exif.org/Exif2-2.PDF')
-;; 
+;;
 ;; metadata namespaces:
 ;; :SEE (URL `http://www.digikam.org/ns/kipi/1.0/')
 ;; :SEE (URL `http://ns.microsoft.com/photo/1.0/')
 ;; :SEE (URL `http://ns.iview-multimedia.com/mediapro/1.0/')
 ;; :SEE (URL `http://ns.microsoft.com/expressionmedia/1.0/')
 ;; :SEE (URL `http://ns.useplus.org/go.ashx')
-;; 
+;;
 ;;; (defclass dbc-image-class-metadata-corefs (base-image)
 ;;   ((class-object-metadata-source 
 ;;    :documentation "The class object with corefs. This is a class object describing a metadata scheme.")
@@ -293,11 +293,11 @@ image-theme-entity
 ;;                     http://purl.org/dc/elements/1.1/
 ;;                    Share similiar fields copyright/IP Fields, e.g.IPTC "Copyright Notice" and DC "Rights"
 ;;                       ")
-;;    
+;;
 ;; dbc-image-metadata-dublin-core
 ;; dbc-image-metadata-exif
 ;; dbc-image-metadata-xmp
-;; 
+;;
 
 ;;; ==============================
 
@@ -312,31 +312,31 @@ image-theme-entity
 ;; exiv2 enumerated the following table columns:
 ;; Property, Label, Value type,  Exiv2 type, Category, Description
 ;; We translated this table to CLOS slots each of which have the the following form:
-;; 
+;;
 ;;  ( ;; Label, Value, c-type, Exiv2-type, Category,  Description
-;;   <PROPERTY>   
+;;   <PROPERTY>
 ;;   ;; :type <CL-TYPE>
 ;;   <DESCRIPTION> "CL-documentation")
-;; In the above form 
+;; In the above form
 ;; - The initial commented comma separated list enumerates the original (unused)
 ;;   values from the exiv2 table
 ;; - <PROPERTY> => slot-name
 ;; - The commented :type form inidicates a CL type we initially expect to
 ;;   implement - we assume this may change as details emerge.
-;; - <DESCRIPTION> => :documentation 
+;; - <DESCRIPTION> => :documentation
 ;;
 ;; (defclass dbc-image-metadata-dublin-core (base-image)
 (
  ( ;; Contributor, bag ProperName, XmpBag, External
   dc-contributor
   ;; :type 'string
-  :documentation 
+  :documentation
   "A string identifying contributors to the resource (other than the authors).")
 
  ( ;; Coverage, Text, XmpText, External
   dc-coverage
   ;; :type 'string
-  :documentation 
+  :documentation
   "A string identifying the spatial or temporal topic of the resource, the spatial
 applicability of the resource, or the jurisdiction under which the resource is
 relevant.")
@@ -344,37 +344,37 @@ relevant.")
  ( ;; Creator, seq, ProperName, XmpSeq, External
   dc-creator
   ;; :type 'list | sequence
-  :documentation 
+  :documentation
   "A list of zero or more strings each identifying the authors of the resource
 \(listed in order of precedence, if significant\).")
 
  ( ;; Date, seq, Date, XmpSeq, External
   dc-date
-  ;; :type ??? 
-  :documentation 
+  ;; :type ???
+  :documentation
   "A sequence of date identifiers.
 Date(s) that something interesting happened to the resource.")
 
  ( ;; Description, Lang Alt, LangAlt, External
   dc-description
   ;; :type 'string
-  :documentation 
+  :documentation
   "A list of zero or more strings describing the content of the resource. 
 Multiple values may be present for different languages.")
 
  ( ;; Format, MIMEType, XmpText, Internal
   dc-format
   ;; :type pathname-type
-  :documentation 
+  :documentation
   "A CL:PATHNAME-TYPE indicating the file format used when saving the resource.
 The type should correpsond to a known mime-type.
-Tools and applications should set this property to the save format of the data. 
+Tools and applications should set this property to the save format of the data.
 It may include appropriate qualifiers.")
 
  ( ;; Identifier, Text, XmpText, External
   dc-identifier
   ;; :type 'unique-universal-identifier
-  :documentation 
+  :documentation
   "Unique identifier of the resource.
 Recommended best practice is to identify the resource by means of a string
 conforming to a formal identification system.")
@@ -382,24 +382,24 @@ conforming to a formal identification system.")
  ( ;; Language, bag Locale, XmpBag, Internal
   language
   ;; :type
-  :documentation 
+  :documentation
   "An unordered array specifying the languages used in the resource.")
 
  ( ;; Publisher, bag ProperName, XmpBag, External
   publisher
   ;; :type
-  :documentation 
-  "An entity responsible for making the resource available. 
-Examples of a Publisher include: 
- - a person; 
- - an organization; 
- - a service. 
+  :documentation
+  "An entity responsible for making the resource available.
+Examples of a Publisher include:
+ - a person;
+ - an organization;
+ - a service.
 Typically, the name of a Publisher should be used to indicate the entity.")
 
  ( ;; Relation, bag Text, XmpBag, Internal
   dc-relation
   ;; :type
-  :documentation 
+  :documentation
   "Relationships to other documents.
 Recommended best practice is to identify the related resource by means of a
 string conforming to a formal identification system.")
@@ -407,7 +407,7 @@ string conforming to a formal identification system.")
  ( ;; Rights, Lang Alt, LangAlt, External
   ;; :type
   dc-rights
-  :documentation 
+  :documentation
   "Informal rights statement, selected by language. 
 Typically, rights information includes a statement about various property rights
 associated with the resource, including intellectual property rights.")
@@ -415,25 +415,25 @@ associated with the resource, including intellectual property rights.")
  ( ;; Source, Text, XmpText, External
   dc-source
   ;; :type
-  :documentation 
+  :documentation
   "Unique identifier of the work from which this resource was derived.")
 
  ( ;; Subject, bag Text, XmpBag, External
   dc-subject
   ;; :type
-  :documentation 
+  :documentation
   "An unordered set of descriptive phrases or keywords that specify the topic of the content of the resource.")
 
  ( ;; Title, Lang Alt, LangAlt,  External
   dc-title
   ;; :type
-  :documentation 
+  :documentation
   "The title of the document, or the name given to the resource. 
 Typically, it will be a name by which the resource is formally known.")
 
  ( ;; Type, bag open, Choice XmpBag, External
   dc-type
-  ;; :type 
+  ;; :type
   :documentation "A document type; for example, novel, poem, or working paper.")
 
  )
@@ -446,7 +446,7 @@ Typically, it will be a name by which the resource is formally known.")
  Property     |  Label    | Value type     | Exiv2 type | Category
  ------------------------------------------------------------------
  CONTRIBUTOR   Contributor  bag ProperName   XmpBag       External     
- COVERAGE      Coverage     Text             XmpText      External        
+ COVERAGE      Coverage     Text             XmpText      External
  CREATOR       Creator      seq ProperName   XmpSeq       External
  DATE          Date         seq Date         XmpSeq       External
  DESCRIPTION   Description  Lang Alt         LangAlt      External
@@ -463,8 +463,8 @@ Typically, it will be a name by which the resource is formally known.")
 
  -----------------------------------------------------------------
 
- CONTRIBUTOR   
- Contributors to the resource (other than the authors). 
+ CONTRIBUTOR
+ Contributors to the resource (other than the authors).
 
  COVERAGE
  The spatial or temporal topic of the resource, the spatial applicability of the
@@ -520,7 +520,7 @@ Typically, it will be a name by which the resource is formally known.")
 
  TYPE
  A document type; for example, novel, poem, or working paper.
- 
+
 |#
 
 
@@ -530,7 +530,6 @@ Typically, it will be a name by which the resource is formally known.")
 
 ;; Local Variables:
 ;; indent-tabs-mode: nil
-;; show-trailing-whitespace: t
 ;; mode: lisp-interaction
 ;; End:
 
