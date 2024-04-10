b66f2f4e9400e68065b641c89a1e9887919263f6
S Pearman <monkpearman@icloud.com>
2024-04-10 15:17:41 -0400

    whitespace

--- dbc-classes/dbc-class-parsed-slot-value-format.lisp
+++ dbc-classes/dbc-class-parsed-slot-value-format.lisp
@@ -3,6 +3,7 @@
 ;;; ==============================
 
 #|
+
 methods for formatting slot-values when exporting to non-lispy files e.g. csv
 
 parsed-class-slot-value-format-keyword-sequenced-entity-coref
@@ -28,11 +29,22 @@ this needs to be delimited for mystuff2 as a group/location - i prefer location
 keyword-seo-sequenced-entity-coref
 
 
+
+;; description-inventory-condition
+
 publication-date-range
 
 publication-date
 NIL | (:YEAR INTEGER :MONTH INTEGER | NIL :DAY INTEGER | NIL)
 we should export both the sequence and as a formatted date-string if all values :MONTH :DAY :YEAR are present
+
+
+
+price-ask
+
+price-sold
+
+ (format nil "~$" 55)
 
 |#
 
@@ -43,8 +55,16 @@ we should export both the sequence and as a formatted date-string if all values 
 
 (defgeneric parsed-class-slot-value-format-image-pathnames (object))
 
-(defgeneric parsed-class-slot-value-format-delimited-string-list (object slot-name delimiter))
+(defgeneric parsed-class-slot-value-format-delimited-string-list (object slot-name &key stream delimiter))
 
+;; prefix should default to #\$
+(defgeneric parsed-class-slot-value-format-price (object slot-name &key stream prefix))
+
+;; 
+(defgeneric parsed-class-slot-value-format-unit-dimension (object slot-name &key stream))
+
+
+(defgeneric parsed-class-slot-value-format-date-plist (object slot-name &key stream date-style allow-empty-month-day))
 
 
 
